36042d3625b7a9f3623bd6860c91d5634b0241e0
Александр <asamatokhin@gmail.com>
2024-02-05 18:31:58 +0300

    add lesson1:task1 and homework lines 1-9

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5c0bab211181a2f1aada65430c25af892b793f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01954d6c0cc532cff7b1845c27129e546ea8435a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
